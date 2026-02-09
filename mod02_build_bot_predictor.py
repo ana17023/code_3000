c0eb1b9ad7343f16d901ef277357533a21ad0a36
ana17023 <antonio.ayala@uconn.edu>
2026-02-09 13:44:18 -0600

    Update mod02_build_bot_predictor.py

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,12 +10,12 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
+        learning_rate=0.2,
+        n_estimators=101,
         #Max depth is the main one to change, but change all of the numbers around to get better or worse models
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        max_depth=9,
+        subsample=2,
+        min_samples_leaf=2,
         random_state=seed
     )
     model.fit(X, y)
